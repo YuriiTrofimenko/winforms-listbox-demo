7830a11ea0889ba1940ef709fd40905c58134c55
student <student@DESKTOP-L3ET5DL>
2020-01-28 18:55:35 +0200

    init n demo

--- ListBoxDemo/Form1.Designer.cs
+++ ListBoxDemo/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.demoListBox = new System.Windows.Forms.ListBox();
+            this.stringTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteItemsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // demoListBox
+            // 
+            this.demoListBox.FormattingEnabled = true;
+            this.demoListBox.Location = new System.Drawing.Point(13, 13);
+            this.demoListBox.Name = "demoListBox";
+            this.demoListBox.Size = new System.Drawing.Size(120, 407);
+            this.demoListBox.TabIndex = 0;
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.Location = new System.Drawing.Point(140, 13);
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(100, 20);
+            this.stringTextBox.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(140, 40);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteItemsButton
+            // 
+            this.deleteItemsButton.Location = new System.Drawing.Point(221, 40);
+            this.deleteItemsButton.Name = "deleteItemsButton";
+            this.deleteItemsButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteItemsButton.TabIndex = 3;
+            this.deleteItemsButton.Text = "Delete";
+            this.deleteItemsButton.UseVisualStyleBackColor = true;
+            this.deleteItemsButton.Click += new System.EventHandler(this.deleteItemsButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteItemsButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.stringTextBox);
+            this.Controls.Add(this.demoListBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox demoListBox;
+        private System.Windows.Forms.TextBox stringTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteItemsButton;
     }
 }
 
--- ListBoxDemo/Form1.cs
+++ ListBoxDemo/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,9 +14,59 @@ namespace ListBoxDemo
 {
     public partial class Form1 : Form
     {
+        // private List<string> strings;
+        // private BindingList<string> strings;
+        private BindingList<dynamic> users;
         public Form1()
         {
             InitializeComponent();
+            /* strings = new BindingList<string> {
+                "Hello",
+                "WinForms"
+            }; */
+            users = new BindingList<dynamic> {
+                new { Id = 1, Name = "Name1", Pasword = "P1", Created_at = DateTime.Now },
+                new { Id = 2, Name = "Name2", Pasword = "P2", Created_at = DateTime.Now }
+            };
+            // demoListBox.Items.AddRange(strings.ToArray());
+            demoListBox.DataSource = users;
+            demoListBox.DisplayMember = "Name";
+            demoListBox.ValueMember = "Id";
+            /* demoListBox.SelectionMode = SelectionMode.One;
+            demoListBox.SelectedIndexChanged += (s, a) => {
+                Console.WriteLine(demoListBox.SelectedItem);
+            };*/
+
+            demoListBox.SelectionMode = SelectionMode.MultiExtended;
+            /*demoListBox.MultiColumn = true;
+            demoListBox.Items.AddRange(new object[] {
+            "Item 1, column 1, column 3",
+            "Item 2, column 1, column 3",
+            "Item 3, column 1, column 3",
+            "Item 4, column 1, column 3",
+            "Item 5, column 1, column 3",
+            "Item 1, column 2, column 3",
+            "Item 2, column 2, column 3",
+            "Item 3, column 2"});*/
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //strings.Add(stringTextBox.Text);
+            users.Add(stringTextBox.Text);
+            // demoListBox.Items.Add(stringTextBox.Text);
+        }
+
+        private void deleteItemsButton_Click(object sender, EventArgs e)
+        {
+            // v1
+            /*List<dynamic> toRemove = new List<dynamic>();
+            foreach (var item in demoListBox.SelectedItems)
+            {
+                toRemove.Add(item);
+            }
+            toRemove.ForEach((toRemoveItem) => { users.Remove(toRemoveItem); });*/
+            // v2
         }
     }
 }
